c55dd7af092522c96bf6dd4a2bc8110f46a8897f
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-04-08 16:06:32 +0100

    Refactor test cases to directly use getSum and getSubtract functions

--- weekFour/calc.test.js
+++ weekFour/calc.test.js
@@ -1,15 +1,15 @@
-const calculate = require("./calc");
+const {getSum, getSubtract} = require("./calc");
 
 test('calculates the sum of 1 and 2 to equal 3', () => {
     const digitOne = 4;
     const digitTwo = 5;
-    const result =  calculate.getSum(digitOne, digitTwo);
+    const result =  getSum(digitOne, digitTwo);
     expect(result).toBe(9);
 });
 
-test('calculates the sum of 1 and 2 to equal 3', () => {
+test('calculates the difference of 4 and 5 to equal -1', () => {
     const digitOne = 4;
     const digitTwo = 5;
-    const result =  calculate.getSubtract(digitOne, digitTwo);
+    const result =  getSubtract(digitOne, digitTwo);
     expect(result).toBe(-1);
 });
