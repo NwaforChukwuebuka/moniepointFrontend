4262f6f1922d4c51730878a4082f1e22c4cc656e
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-04-09 15:33:06 +0100

    Remove commented-out console logs and reorganize product fetching function

--- weekFour/AsynchronousProgramming/async.js
+++ weekFour/AsynchronousProgramming/async.js
@@ -1,12 +1,26 @@
-console.log("I am the first");
+// console.log("I am the first");
 
-setTimeout(() => {
-    console.log("I am the second")
-}, 2000)
 
-setTimeout(() => {
-    console.log("I am the third")
-}, 4000)
+// setTimeout(() => {
+//     console.log("I am the second")
+// }, 2000)
 
-console.log("I am the fourth")   
+// setTimeout(() => {
+//     console.log("I am the third")
+// }, 4000)
+
+// console.log("I am the fourth") 
+
+// Could be GET or POST/PUT/PATCH/DELETE
+
+const PRODUCT_URL = 'https://dummyjson.com/test';
+
+
+
+function getProducts(PRODUCT_URL) {
+    fetch(PRODUCT_URL)
+        .then(res => res.json())
+        .then(console.log);
+}
+getProducts(PRODUCT_URL);
 
