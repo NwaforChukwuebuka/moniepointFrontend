6e3a8c7dd73939893b4f9e8f54a7eb84df12d00c
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-04-09 15:19:21 +0100

    Add asynchronous logging example with setTimeout

---
+++ weekFour/AsynchronousProgramming/async.js
@@ -0,0 +1,12 @@
+console.log("I am the first");
+
+setTimeout(() => {
+    console.log("I am the second")
+}, 2000)
+
+setTimeout(() => {
+    console.log("I am the third")
+}, 4000)
+
+console.log("I am the fourth")   
+
