1fefaa77f24599f3c109c0a5ea892121d5b2f966
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-04-09 11:52:29 +0100

    Add Task Manager module with HTML, CSS, and JavaScript files

---
+++ weekFour/taskManager/script.js
@@ -0,0 +1,17 @@
+// const wrapper = document.getElementById('wrapper');
+// console.log(wrapper);
+
+// const title = document.getElementsByClassName('title')
+// console.log(title) //Output: HTMLCollection(1) [h2.title]
+
+
+// const tags = document.getElementsByTagName('li')
+// console
+
+// //querySelector() - returns the first element that matches a specified CSS selector(s) in the document
+// const firstTitle = document.querySelector('.title')
+// console.log(firstTask) //Output: <h2 class="title">Task Manager</h2>
+
+//querySelectorAll() - returns a static NodeList of all elements that match a specified CSS selector(s) in the document
+const allTitles = document.querySelectorAll('.title')
+console.log(allTitles) 
